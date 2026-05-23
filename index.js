7b0438b76053c6ac906a03550e06b4e4ca23fb20
shiv07-blip <kumarishivangi65782@gmail.com>
2026-05-23 11:30:23 +0530

    added root route

--- index.js
+++ index.js
@@ -19,7 +19,13 @@ const PORT = process.env.PORT || 3000;
 
 
 // Middlewaresapi/
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://reposensei-93e6.vercel.app',  // your Vercel URL
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: '10mb' }));
 
 // Routes
