1457cf49232f94dbc8fb76dc67e21149ef3974ce
shiv07-blip <kumarishivangi65782@gmail.com>
2026-05-23 10:54:57 +0530

    Added health route

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("RepoSensei Backend Running");
+  res.send("RepoSensei Backend Running 🚀");
 });
 
 // Middlewaresapi/
